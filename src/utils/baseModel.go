72629d6505af48d5898d0b4b0a69328cd27c0949
Fahrul Fauzi <fahrul.fauz@gmail.com>
2021-06-15 16:26:56 +0700

    Fix timestamp model

--- src/utils/baseModel.go
+++ src/utils/baseModel.go
@@ -2,12 +2,9 @@ package utils
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type TimestampModel struct {
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+	CreatedAt time.Time `json:"created_at" gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"default:CURRENT_TIMESTAMP"`
 }
