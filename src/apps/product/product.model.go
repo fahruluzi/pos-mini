4ebe1e1d9889713d21cc90c13e2fc3ee817cff84
Fahrul Fauzi <fahrul.fauz@gmail.com>
2021-06-15 16:24:12 +0700

    Fix integer migration model product

--- src/apps/product/product.model.go
+++ src/apps/product/product.model.go
@@ -9,9 +9,9 @@ type Products struct {
 	UUID         string             `gorm:"type:varchar(36);primaryKey;not null" json:"uuid"`
 	Name         string             `gorm:"type:varchar(255);not null" json:"name"`
 	SKU          string             `gorm:"index:sku_index;type:varchar(255);not null;unique" json:"sku"`
-	Price        string             `gorm:"type:int(16);not null" json:"price"`
-	ActualPrice  string             `gorm:"type:int(16);not null" json:"actual_price"`
-	Stock        string             `gorm:"type:int(16);not null" json:"stock"`
+	Price        int32              `gorm:"not null" json:"price"`
+	ActualPrice  int32              `gorm:"not null" json:"actual_price"`
+	Stock        int32              `gorm:"not null" json:"stock"`
 	Image        string             `gorm:"type:varchar(255);not null" json:"image"`
 	MerchantUuid string             `gorm:"type:varchar(36);not null" json:"merchant_uuid"`
 	Merchant     merchant.Merchants `gorm:"foreignKey:MerchantUuid;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
